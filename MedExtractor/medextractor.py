8a858bc8519646b8f8d638520a5f8a92f102da6b
Dietrich Toennies <dietrich2003@t-online.de>
2022-12-23 09:40:08 +0100

    KnowledgeExtractor etwas aufgeräumt und dafür medextractor etwas ergänzt.

--- MedExtractor/medextractor.py
+++ MedExtractor/medextractor.py
@@ -14,12 +14,23 @@ preprocessor = RuleBasedPreprocessor(text_to_analyze)
 preprocessed_text = preprocessor.get_preprocessed_text()
 print(preprocessed_text)
 nlp = spacy.load('en_core_web_sm')
-knowledgeExtractor = KnowledgeExtractor('Medextractor\\test.kb',nlp)
+knowledgeExtractor = KnowledgeExtractor('MedExtractor\\test.kb',nlp)
+
+span = nlp("the blues")[0:2]
+span.label_ = "DISEASE"
+
+context = set()
+context.add(span)
+knowledgeExtractor.set_context(context)
+
 for sent in nlp(preprocessed_text).sents:
-    print(sent.text)
     knowledgeExtractor(sent.text)
 
 knowledgebase = knowledgeExtractor.get_knowledge_base()
+
+for semantic_relation in knowledgebase._semantic_relations:
+    print(semantic_relation.__str__())
+
 # print(knowledgebase)
 # rdfSerialiser = DummyRDFSerialiser(knowledgebase)
 # print(rdfSerialiser.serialise_knowledgebase())
