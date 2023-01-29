3360ab98b9763875fef2bf2b691984f9a026c2bc
claraja <jansen.clara@t-online.de>
2023-01-29 13:55:50 +0100

    Formatierung + Fehlerbehebung: diseases nicht mehr als symptom

--- MedExtractor/src/rdf/graphmanager.py
+++ MedExtractor/src/rdf/graphmanager.py
@@ -1,11 +1,11 @@
-from rdflib import Graph, Literal,Namespace
+from rdflib import Graph, Namespace
 from rdflib.term import URIRef
 
 
 class GraphManager:
     def __init__(self, namespace_prefix, namespace_uri):
-        self.namespace_prefix=namespace_prefix
-        self.namespace_uri=namespace_uri
+        self.namespace_prefix = namespace_prefix
+        self.namespace_uri = namespace_uri
         self.namespace = Namespace(self.namespace_uri)
         self.graph = Graph()
         self.namespace_manager = self.graph.namespace_manager
@@ -13,21 +13,13 @@ class GraphManager:
         self.diseases = {}
         self.hasSymptom = URIRef(self.namespace_uri + "hasSymptom")
 
-    def add_disease(self, disease):
-        if disease in self.diseases:
-            pass
-        else:
-            disease_URI = URIRef(self.namespace_uri + disease)
-            self.diseases[disease] = disease_URI
-
-
     def add_symptom(self, disease, symptom):
-        symptom_URI = URIRef(self.namespace_uri + symptom)
-        if disease in self.diseases:
-            disease_URI = self.diseases.get(disease)
+        symptom_URI = URIRef(self.namespace_uri + f"symptom:{symptom}")
+        if f"disease:{disease}" in self.diseases:
+            disease_URI = self.diseases.get(f"disease:{disease}")
         else:
-            disease_URI = URIRef(self.namespace_uri + disease)
-            self.diseases[disease] = disease_URI
+            disease_URI = URIRef(self.namespace_uri + f"disease:{disease}")
+            self.diseases[f"disease:{disease}"] = disease_URI
         self.graph.add((disease_URI, self.hasSymptom, symptom_URI))
 
     def get_serialized_graph(self, output_path, serialization_format='pretty-xml'):
