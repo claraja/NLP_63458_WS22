730d9d0e485154e540cf91dcd15acc527f7d25b4
claraja <jansen.clara@t-online.de>
2023-01-30 00:02:50 +0100

    kleine Verbesserungen Preprocessor

--- MedExtractor/src/preprocessor/preprocessor.py
+++ MedExtractor/src/preprocessor/preprocessor.py
@@ -24,23 +24,27 @@ class RuleBasedPreprocessor(PreprocessingInterface):
         nlp = spacy.blank('en')
         nlp.add_pipe('sbd', first=True)
 
-        our_punctuation = punctuation+' •–'
+        our_punctuation = punctuation+' •–-'
         with open(self.doc_name, 'r', encoding='utf-8') as file:
             raw_text = file.read()
+
+        # Aufzählungen sollen als einzelne Sätze gefunden werden
+        raw_text = raw_text.replace('•', '\n-')
+        # eckige Klammern mit Ziffern (z.B. bei Literaturangaben) entfernen
+        raw_text = re.sub('\[\d+\]', '', raw_text)
 
         doc = nlp(raw_text)
 
         enumeration, new_raw_text, bullet_point = '', '', ''
-        for sent_id, sent in enumerate(doc.sents, start=1):
+        for _, sent in enumerate(doc.sents, start=1):
             sentence = sent.text
             # delete trailing whitespace
             sentence = sentence.rstrip()
-            # print(sent_id, sentence, sep='\t|\t')
             # ignore empty lines
             if (sentence == ''):
                 pass
             # remember start of enumeration
-            elif sentence[-1] == ':':
+            elif sentence[-1] in [':', '—']:
                 enumeration = sentence[0:-1]
                 continue
             elif ((sentence[-1] not in ['.', '!', '?', ':'])
@@ -50,7 +54,7 @@ class RuleBasedPreprocessor(PreprocessingInterface):
             elif ((sentence[-1] not in ['.', '!', '?', ':'])
                     and (enumeration != '')):
                 sentence = sentence.lstrip()
-                # wenn bullet_point exists sollte er bei allen Teilen
+                # wenn bullet_point existiert sollte er bei allen Teilen
                 # der Aufzählung gleich sein
                 if bullet_point != '':
                     if sentence.startswith(bullet_point):
@@ -63,13 +67,7 @@ class RuleBasedPreprocessor(PreprocessingInterface):
                         # Aufzählungszeichen herauszubekommen
                         sentence_tmp = sentence
 
-                        # In der While-Schleife wurde die Bedingung   sentence_tmp != ''   ergänzt.
-                        # Grund: pyspd scheint Probleme mit den Literaturangaben wie z.B. '[1]' zu haben.
-                        # Es kommt vor, dass die schließende Klammer nicht mehr als Teil des Satzes gesehen wird.
-                        # Dann wird aus ']' ein einzelner Satz. Wird dann das Zeichen ']' entfernt, ist der
-                        # verbleibende String leer. Alternativ könnte man alle Literaturangaben aus
-                        # dem Text entfernen, bevor spaCy zum Einsatz kommt.
-                        while (sentence_tmp != '') and ((sentence_tmp[0] in our_punctuation) | (sentence_tmp[0] == ' ')):
+                        while ((sentence_tmp[0] in our_punctuation) | (sentence_tmp[0] == ' ')):
                             sentence_tmp = sentence_tmp[1:]
                         # speichere Aufzählungszeichen falls vorhanden
                         if len(sentence) != len(sentence_tmp):
@@ -84,8 +82,8 @@ class RuleBasedPreprocessor(PreprocessingInterface):
             else:
                 enumeration, bullet_point = '', ''
             # replace all occurrences of multiple spaces with a single space
-            result = re.sub(' +', ' ', sentence)
-            new_raw_text += f'{result}.\n'
+            processed_sentence = re.sub(' +', ' ', sentence)
+            new_raw_text += f'{processed_sentence}.\n'
         # further replacements
         new_raw_text = new_raw_text\
             .replace('..', '.')\
@@ -93,8 +91,8 @@ class RuleBasedPreprocessor(PreprocessingInterface):
             .replace('!.', '!')\
             .replace('\n ', '\n')\
             .replace('_', '')\
-            .replace(' - ', ' ')\
-            .replace(' – ', ' ')
+            .replace('-', ' ')\
+            .replace('–', ' ')
         # replace again possible occurrences of multiple spaces with a single space
         new_raw_text = re.sub(' +', ' ', new_raw_text)
         return new_raw_text
