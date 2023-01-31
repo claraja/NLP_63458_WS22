239b945ea9c8ba6e8d7ba18a7d533fe7f3fc89be
DeepTranslation <anne261@gmx.de>
2023-01-31 16:12:31 +0100

    manuelles merging; doku neu erstellt.

--- MedExtractor/medextractor/rdf/RDFSerialiser.py
+++ MedExtractor/medextractor/rdf/RDFSerialiser.py
@@ -6,14 +6,46 @@ from knowledge.relations import RelationType
 class RDFSerialiser(RDFSerialiserInterface):
 
     def serialise_knowledgebase(self, output_path):
+        """Serialise knowledge base into RDF file
+
+        Parameters:
+        ----------
+        output_path: string
+            path to which the xml-document resulting from the rdflib-Graph will be saved
+
+        Returns:
+        -------
+        None
+        """
         self.knowledgebase_to_graph()
         self._graphmanager.get_serialized_graph(
             output_path, self._serialisation_format)
 
     def set_serialisation_format(self, serialisation_format):
+        """Gives the possibility to change the serialisation format to which the rdflib-graph is serialised
+        Possible formats: see https://rdflib.readthedocs.io/en/stable/intro_to_parsing.html#saving-rdf
+
+        Parameters:
+        ----------
+        serialisation_format: string
+
+        Returns:
+        -------
+        None
+        """
         self._serialisation_format = serialisation_format
 
     def knowledgebase_to_graph(self):
+        """Transfers the content of the knowledgebase into a rdflib-graph
+
+        Parameters:
+        ----------
+        None
+
+        Returns:
+        -------
+        None
+        """
         for semantic_relation in self._knowledgebase.semantic_relations:
             entity_1_name = semantic_relation.entity_1.entity_name.replace(
                 ' ', '_')
