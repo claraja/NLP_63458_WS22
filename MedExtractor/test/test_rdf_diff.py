2315724a886f83e7b023f5fd1e671b218c32c7b2
DeepTranslation <anne261@gmx.de>
2023-02-12 16:42:24 +0100

    test rdf diff

---
+++ MedExtractor/test/test_rdf_diff.py
@@ -0,0 +1,98 @@
+from rdflib import Graph
+from rdflib.compare import to_isomorphic, graph_diff
+from rdflib import Namespace, Graph, URIRef, Literal
+
+def dump_nt_sorted(g):
+    print()
+    for l in sorted(g.serialize(format='nt').splitlines()):
+        if l: print(l)
+
+def add_symptom(symptom_str: str, graph: Graph):
+    symptom_uri = URIRef(namespace_string + symptom_str.replace(' ', '_'))
+    graph.add((depression, hasSymptom, symptom_uri))
+
+# g1 = Graph().parse(format='n3', data='''
+#          @prefix : <http://example.org/ns#> .
+#          <http://example.org> :rel
+#              <http://example.org/same>,
+#              [ :label "Same" ],
+#              <http://example.org/a>,
+#              [ :label "A" ] .
+#      ''')
+#
+#
+# g2 = Graph().parse(format='n3', data='''
+#          @prefix : <http://example.org/ns#> .
+#          <http://example.org> :rel
+#              <http://example.org/same>,
+#              [ :label "Same" ],
+#              <http://example.org/b>,
+#              [ :label "B" ] .
+#      ''')
+#
+# iso1 = to_isomorphic(g1)
+# iso2 = to_isomorphic(g2)
+#
+# print(iso1 == iso2)
+#
+# in_both, in_first, in_second = graph_diff(iso1, iso2)
+#
+# print(in_both)
+# print(in_first)
+# print(in_second)
+#
+# dump_nt_sorted(in_both)
+# dump_nt_sorted(in_first)
+# dump_nt_sorted(in_second)
+#
+#
+
+
+
+
+if __name__ == "__main__":
+
+    namespace_string = "http://fapranlp.de/"
+    namespace = Namespace(namespace_string)
+    output_path = "../resources/manual_text_to_analyze.xml"
+    prefix = "fapra"
+    # Create a Graph
+    g3 = Graph()
+    namespace_manager3 = g3.namespace_manager
+    namespace_manager3.bind(prefix, namespace)
+    g4 = Graph()
+    namespace_manager4 = g4.namespace_manager
+    namespace_manager4.bind(prefix, namespace)
+
+    depression = URIRef(namespace_string + "depression")
+    hasSymptom = URIRef(namespace_string + "hasSymptom")
+
+    symptoms3 = ["continuous low mood", "sadness", "feeling hopeless and helpless", ]
+    symptoms4 = ["sadness", "feeling hopeless and helpless", "low self-esteem",
+                 "feeling tearful"]
+
+    for symptom in symptoms3:
+        add_symptom(symptom, g3)
+
+
+    for symptom in symptoms4:
+        add_symptom(symptom, g4)
+
+    iso1 = to_isomorphic(g3)
+    iso2 = to_isomorphic(g4)
+
+    print(iso1 == iso2)
+
+    in_both, in_first, in_second = graph_diff(iso1, iso2)
+
+    print(in_both)
+    print(in_first)
+    print(in_second)
+
+    dump_nt_sorted(in_both)
+    dump_nt_sorted(in_first)
+    dump_nt_sorted(in_second)
+
+    print(f"Graph in_both has {len(in_both)} statements.")
+    print(f"Graph in_first has {len(in_first)} statements.")
+    print(f"Graph in_second has {len(in_second)} statements.")
